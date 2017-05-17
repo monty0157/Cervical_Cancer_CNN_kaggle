7fe5f57bbd4da83e72bf8fb9f7f8d2343d1347ce
monty0157 <kaspermz@hotmail.com>
2017-05-17 22:21:43 +0200

    Added more folders to remove_exif: Needs reformatting

--- data_processing.py
+++ data_processing.py
@@ -3,13 +3,25 @@ from keras.preprocessing import image
 import random
 import numpy as np
 
-type1_path_train = './data/train/Type_1/'
-type1_list_train = os.listdir(type1_path_train)
+Type_1_path_train = './data/train/Type_1/'
+Type_1_list_train = os.listdir(Type_1_path_train)
 type2_path_train = './data/train/Type_2/'
 type2_list_train = os.listdir(type2_path_train)
 type3_path_train = './data/train/Type_3/'
 type3_list_train = os.listdir(type3_path_train)
 
+type1_path_val = './data/validation/Type_1/'
+type1_list_val = os.listdir(type1_path_val)
+type2_path_val = './data/validation/Type_2/'
+type2_list_val = os.listdir(type2_path_val)
+type3_path_val = './data/validation/Type_3/'
+type3_list_val = os.listdir(type3_path_val)
+
+test_path_train = './data/test/'
+test_list_train = os.listdir(test_path_train)
+
+train_directories = ['Type_1']
+val_directories = ['Type_1', 'Type_3']
 #PREPARING DATA FOR GRID SEARCH
 def grid_search_helper(target_size):
 
@@ -48,41 +60,91 @@ def grid_search_helper(target_size):
 def remove_exif():
     from PIL import Image, ImageFile
     ImageFile.LOAD_TRUNCATED_IMAGES = True
-    
-    for file in type1_list_train:
-        if (file != '.DS_Store'):
 
-            image = Image.open(type1_path_train + file)
-        
-            # next 3 lines strip exif
-            data = list(image.getdata())
-            image_without_exif = Image.new(image.mode, image.size)
-            image_without_exif.putdata(data)
-        
-            image_without_exif.save('./data/train_no_exif/Type_1/' + file)
-            
+    '''for i in range(train_directories):
+        for file in train_directories[i]:
+            if (file != '.DS_Store'):
+
+                image = Image.open((train_directories[i] + _path_train) + file)
+
+                # next 3 lines strip exif
+                data = list(image.getdata())
+                image_without_exif = Image.new(image.mode, image.size)
+                image_without_exif.putdata(data)
+
+                image_without_exif.save('./data/train_no_exif/' + train_directories[i] + '/' + file)'''
+
     for file in type2_list_train:
         if (file != '.DS_Store'):
 
             image = Image.open(type2_path_train + file)
-        
+
             # next 3 lines strip exif
             data = list(image.getdata())
             image_without_exif = Image.new(image.mode, image.size)
             image_without_exif.putdata(data)
-        
+
             image_without_exif.save('./data/train_no_exif/Type_2/' + file)
-            
-    for file in type3_list_train:
+
+    '''for file in type3_list_train:
         if (file != '.DS_Store'):
 
             image = Image.open(type3_path_train + file)
-        
+
             # next 3 lines strip exif
             data = list(image.getdata())
             image_without_exif = Image.new(image.mode, image.size)
             image_without_exif.putdata(data)
-        
-            image_without_exif.save('./data/train_no_exif/Type_3/' + file)
-            
+
+            image_without_exif.save('./data/train_no_exif/Type_3/' + file)'''
+
+    '''for file in test_list_train:
+        if (file != '.DS_Store'):
+
+            image = Image.open(test_path_train + file)
+
+            # next 3 lines strip exif
+            data = list(image.getdata())
+            image_without_exif = Image.new(image.mode, image.size)
+            image_without_exif.putdata(data)
+
+            image_without_exif.save('./data/test_no_exif/' + file)'''
+
+
+    '''for file in type1_list_val:
+        if (file != '.DS_Store'):
+
+            image = Image.open(type1_path_val + file)
+
+            # next 3 lines strip exif
+            data = list(image.getdata())
+            image_without_exif = Image.new(image.mode, image.size)
+            image_without_exif.putdata(data)
+
+            image_without_exif.save('./data/validation_no_exif/Type_1/' + file)'''
+
+    for file in type2_list_val:
+        if (file != '.DS_Store'):
+
+            image = Image.open(type2_path_val + file)
+
+            # next 3 lines strip exif
+            data = list(image.getdata())
+            image_without_exif = Image.new(image.mode, image.size)
+            image_without_exif.putdata(data)
+
+            image_without_exif.save('./data/validation_no_exif/Type_2/' + file)
+
+    '''for file in type3_list_val:
+        if (file != '.DS_Store'):
+
+            image = Image.open(type3_path_val + file)
+
+            # next 3 lines strip exif
+            data = list(image.getdata())
+            image_without_exif = Image.new(image.mode, image.size)
+            image_without_exif.putdata(data)
+
+            image_without_exif.save('./data/validation_no_exif/Type_3/' + file)'''
+
 remove_exif()
